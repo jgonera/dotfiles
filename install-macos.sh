3f263bef1f574a89e3f94e2b9dc1d9b50dfa3267
Juliusz Gonera <jgonera@gmail.com>
2019-10-15 10:13:44 -0700

    Don't install iTerm and skip bash configs
    For now. macOS Catalina uses zsh by default.

--- install-macos.sh
+++ install-macos.sh
@@ -2,20 +2,16 @@
 
 DOTFILES_DIR=$(cd "$(dirname "$0")"; pwd)
 
-brew cask install iterm2
 brew install nvim
 brew install ripgrep
 brew install fzf
 brew install git
 brew install bash-completion
-brew install fasd
 brew install tree
 
 git config --global push.default current
 git config --global rebase.autosquash true
 
-ln -s "$DOTFILES_DIR/data/bashrc" ~/.bashrc
-ln -s "$DOTFILES_DIR/data/bashrc" ~/.bash_profile
 ln -s "$DOTFILES_DIR/data/bin" ~/bin
 ln -s "$DOTFILES_DIR/data/nvim" ~/.config/nvim
 ln -s "$DOTFILES_DIR/data/ripgreprc" ~/.ripgreprc
