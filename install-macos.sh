505e13999d9ebb67fa0a7949485163c21c13ce33
Juliusz Gonera <jgonera@gmail.com>
2019-10-15 11:03:50 -0700

    First zsh config

--- install-macos.sh
+++ install-macos.sh
@@ -2,16 +2,19 @@
 
 DOTFILES_DIR=$(cd "$(dirname "$0")"; pwd)
 
+ln -sfF "$DOTFILES_DIR/data/bin" ~/bin
+ln -sfF "$DOTFILES_DIR/data/nvim" ~/.config/nvim
+ln -sfF "$DOTFILES_DIR/data/ripgreprc" ~/.ripgreprc
+ln -sfF "$DOTFILES_DIR/data/zshrc" ~/.zshrc
+
 brew install nvim
 brew install ripgrep
 brew install fzf
 brew install git
-brew install bash-completion
+brew install zsh-completions
 brew install tree
 
 git config --global push.default current
 git config --global rebase.autosquash true
 
-ln -s "$DOTFILES_DIR/data/bin" ~/bin
-ln -s "$DOTFILES_DIR/data/nvim" ~/.config/nvim
-ln -s "$DOTFILES_DIR/data/ripgreprc" ~/.ripgreprc
+$(brew --prefix)/opt/fzf/install
