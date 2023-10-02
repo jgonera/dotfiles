3e77df038a29f7c4355b26bf8bc160320cdb1b03
Juliusz Gonera <jgonera@gmail.com>
2023-10-01 18:14:53 -0700

    [nvim] Add actionlint for Github Actions

--- data/nvim/init.lua
+++ data/nvim/init.lua
@@ -240,6 +240,7 @@ local null_ls = require("null-ls")
 null_ls.setup({
   on_attach = require("lsp-format").on_attach,
   sources = {
+    null_ls.builtins.diagnostics.actionlint,
     null_ls.builtins.diagnostics.eslint,
     null_ls.builtins.diagnostics.flake8,
     null_ls.builtins.diagnostics.selene,
