10bb50b00284564abc47abc95a48d6765b23d9ac
Juliusz Gonera <jgonera@gmail.com>
2023-10-25 15:50:51 -0700

    [nvim] Add some gitsigns hotkeys

--- data/nvim/init.lua
+++ data/nvim/init.lua
@@ -222,6 +222,29 @@ require("gitsigns").setup({
   signs = {
     untracked = { text = "·" },
   },
+  on_attach = function(bufnr)
+    local gs = package.loaded.gitsigns
+
+    local function map(mode, l, r, opts)
+      opts = opts or {}
+      opts.buffer = bufnr
+      vim.keymap.set(mode, l, r, opts)
+    end
+
+    map("n", "<leader>hr", gs.reset_hunk)
+    map("v", "<leader>hr", function()
+      gs.reset_hunk({ vim.fn.line("."), vim.fn.line("v") })
+    end)
+    map("n", "<leader>hR", gs.reset_buffer)
+    map("n", "<leader>hp", gs.preview_hunk)
+    map("n", "<leader>hb", function()
+      gs.blame_line({ full = true })
+    end)
+    map("n", "<leader>hd", gs.diffthis)
+    map("n", "<leader>hD", function()
+      gs.diffthis("~")
+    end)
+  end,
 })
 
 local capabilities = require("cmp_nvim_lsp").default_capabilities()
