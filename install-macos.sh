9226f5fe6e0dea09705203c790676b26c7f8ddce
Juliusz Gonera <jgonera@gmail.com>
2025-05-16 11:47:38 -0700

    Update install-macos.sh

--- install-macos.sh
+++ install-macos.sh
@@ -19,9 +19,14 @@ ln -sfn "$DOTFILES_DIR/data/ripgreprc" ~/.ripgreprc
 ln -sfn "$DOTFILES_DIR/data/wezterm.lua" ~/.wezterm.lua
 ln -sfn "$DOTFILES_DIR/data/zshrc" ~/.zshrc
 
+echo "Installing Homebrew..."
+
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
 echo "Installing Homebrew packages..."
 
 brew install --cask alacritty
+brew install --cask keepassxc
 brew install --cask wezterm
 brew install bat
 brew install eza
@@ -31,16 +36,17 @@ brew install git
 brew install git-delta
 brew install nvim
 brew install ripgrep
+brew install syncthing
 
 echo "Performing additional configuration..."
 
 $(brew --prefix)/opt/fzf/install
 
 # https://github.com/zsh-users/zsh-completions/issues/433
-sudo chown -R "$USER" /usr/local/share/zsh
-sudo chmod -R 755 /usr/local/share/zsh
+# sudo chown -R "$USER" /usr/local/share/zsh
+# sudo chmod -R 755 /usr/local/share/zsh
 
 # https://github.com/alacritty/alacritty/issues/4616#issuecomment-1236413444
-defaults write -g AppleFontSmoothing -int 0
+# defaults write -g AppleFontSmoothing -int 0
 
 echo "Done."
